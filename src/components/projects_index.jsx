2ab162727136ab04ec3c60ec74e98ab31385dcd8
igor-starostenko <contactigorstar@gmail.com>
2017-12-06 23:09:18 -0800

    some components

---
+++ src/components/projects_index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ProjectsIndex extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Projects</h1>
+      </div>
+    );
+  }
+}
---
+++ src/reducers/user_reducers.js
@@ -0,0 +1,8 @@
+import { LOGIN } from '../actions';
+
+export default function (state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
