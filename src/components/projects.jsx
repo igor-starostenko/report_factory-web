efcd2f996e08d6490d3895d7ad49d78f9fc96720
igor-starostenko <contactigorstar@gmail.com>
2017-12-09 21:01:55 -0800

    wait for projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,13 +5,27 @@ import { getProjects } from '../actions';
 
 class Projects extends Component {
   componentDidMount() {
-    const { cookies } = this.props;
-    const xApiKey = cookies.get('X-API-KEY');
-    this.props.getProjects(xApiKey);
+    this.waitForProjects();
+  }
+
+  getApiKey() {
+    return this.props.cookies.get('X-API-KEY');
+  }
+
+  waitForProjects(times = 10) {
+    const xApiKey = this.getApiKey();
+    if (!xApiKey) {
+      const remaining = times - 1;
+      if (times === 0) {
+        throw new Error('X-API-KEY undefined');
+      }
+      setTimeout(() => { this.waitForProjects(remaining); }, 100);
+    } else {
+      this.props.getProjects(xApiKey);
+    }
   }
 
   render() {
-    console.log(this.props.projects);
     if (this.props.error) {
       return this.props.history.push('/login');
     }
