3bf92fc9183dbff19318ffa3d668c9e7b30262b5
igor-starostenko <contactigorstar@gmail.com>
2018-01-22 00:01:01 -0800

    fixed header for mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -27,6 +27,12 @@ class Navbar extends Component {
           <nav className="navbar navbar-ct-yellow navbar-fixed-top" role="navigation">
             <div className="container">
               <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
                 <Link to="/" className="navbar-brand">Report Factory</Link>
               </div>
               <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
