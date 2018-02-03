539c4dc74f634ebe0d263a2f01cf62f88b945142
igor-starostenko <contactigorstar@gmail.com>
2018-02-03 11:27:34 -0800

    user reports filters

--- src/components/user_reports_line_chart.jsx
+++ src/components/user_reports_line_chart.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 import { Line as LineChart } from 'react-chartjs';
 import { getUserReports } from '../actions/users_actions';
-import { lastDays, formatDays, reportsPerDay, reportsCreatedDates,
+import { lastDays, lastMonths, formatDates, reportsPerDay, reportsPerMonth, reportsCreatedDates,
          groupReportsByProjects, getColors, setOpacity  } from '../helpers/chart_helpers';
 
 const parseDate = report => {
@@ -14,7 +14,7 @@ const parseProjectName = report => {
   return _.get(report, 'attributes.report.project_name');
 }
 
-const projectReportsDatasets = (reports, dates) => {
+const projectReportsDatasets = (reports, dates, reportsPerDate) => {
   let datasets = [];
   let colorIndex = 0;
   const colors = getColors();
@@ -30,7 +30,7 @@ const projectReportsDatasets = (reports, dates) => {
 			pointStrokeColor: "#fff",
 			pointHighlightFill: setOpacity(color, 1),
 			pointHighlightStroke: "rgba(220,220,220,1)",
-			data: reportsPerDay(dates, reportsDates)
+			data: reportsPerDate(dates, reportsDates)
 		});
     colorIndex === colorNames.length - 1 ? colorIndex = 0 : colorIndex++;
   });
@@ -38,6 +38,11 @@ const projectReportsDatasets = (reports, dates) => {
 }
 
 class UserReportsLineChart extends Component {
+  constructor(state) {
+    super(state)
+    this.state = { activeFilter: 'Week' }
+  }
+
   componentDidMount() {
     const { reports } = this.props;
     if (!reports || _.isEmpty(reports)) {
@@ -46,18 +51,48 @@ class UserReportsLineChart extends Component {
     }
   }
 
+  setFilter(name) {
+    this.setState({ activeFilter: name });
+  }
+
+  renderFilterItem(name) {
+    const className = this.state.activeFilter === name ? "active" : '';
+    return (
+      <li className={className} onClick={this.setFilter.bind(this, name)}>
+        <a>{name}</a>
+      </li>
+    );
+  }
+
   render() {
-    const dates = lastDays(10);
-    const days = formatDays(dates);
     const reports = groupReportsByProjects(this.props.reports, parseProjectName);
 
     if (_.isEmpty(reports)) {
       return (<div className="loading">Loading</div>);
     }
 
+    let units, dates, datasets;
+    switch (this.state.activeFilter) {
+      case 'Week':
+        units = lastDays(8);
+        dates = formatDates(units);
+        datasets = projectReportsDatasets(reports, units, reportsPerDay);
+        break;
+      case 'Month':
+        units = lastDays(32);
+        dates = formatDates(units);
+        datasets = projectReportsDatasets(reports, units, reportsPerDay);
+        break;
+      case 'Year':
+        units = lastMonths(12);
+        dates = formatDates(units, { month: 'short' });
+        datasets = projectReportsDatasets(reports, units, reportsPerMonth);
+        break;
+    }
+
     const chartData = {
-    	labels: days,
-    	datasets: projectReportsDatasets(reports, dates),
+    	labels: dates,
+    	datasets: datasets,
     };
 
     const options = {
@@ -68,6 +103,13 @@ class UserReportsLineChart extends Component {
     return (
       <div>
         <LineChart data={chartData} options={options} height="320" />
+        <div className="filters">
+          <ul id="chart-pills" className="nav nav-pills ct-orange">
+            {this.renderFilterItem('Year')}
+            {this.renderFilterItem('Month')}
+            {this.renderFilterItem('Week')}
+          </ul>
+        </div>
       </div>
     );
   }
