12524745db5624c4f1c9f4943f6f1c01bf7d9b8e
igor-starostenko <contactigorstar@gmail.com>
2018-03-01 20:31:40 -0800

    reports details responsive styling

--- src/components/rspec_report_details.jsx
+++ src/components/rspec_report_details.jsx
@@ -1,38 +1,40 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 import { formatDuration, formatDateAgo } from '../helpers/format_helpers';
 import styles from './styles/RspecReportDetails.css';
 
-// const statusName = (failureCount) => {
-//   if (failureCount > 0) {
-//     return 'failedTest';
-//   }
-//   return 'passedTest';
-// };
-
 export default class RspecReportDetails extends Component {
-  render() {
-    if (!this.props.report) {
-      return (<div className="loading">Loading...</div>);
-    }
-
-    const { date, summary } = this.props.report.attributes;
-    // const status = statusName(summary.failure_count);
-    const details = {
-      Project: this.props.report.attributes.project_name,
-      Type: this.props.report.attributes.report_type,
-      Ran: `${formatDateAgo(new Date(date.created_at))} ago`,
-      Duration: formatDuration(summary.duration),
-      'Total Count': summary.example_count,
-      Pending: summary.pending_count,
-      Failed: summary.failure_count,
-    };
-
+  static renderDetails(details) {
     return _.map(details, (value, key) => (
       <div className={styles.reportDetailsRow} key={key}>
         <div className={styles.reportDetailsParam}>{key}:</div>
         <div className={styles.reportDetailsValue}>{value}</div>
       </div>
     ));
+  }
+
+  render() {
+    const { date, summary } = this.props.report.attributes;
+    const projectName = this.props.report.attributes.project_name;
+    const projectLink = `/projects/${projectName}`;
+    const details = {
+      Type: this.props.report.attributes.report_type,
+      Ran: `${formatDateAgo(new Date(date.created_at))} ago`,
+      Duration: formatDuration(summary.duration),
+      'Total Examples': summary.example_count,
+      Pending: summary.pending_count,
+      Failed: summary.failure_count,
+    };
+
+    return (
+      <div>
+        <div className={styles.reportDetailsRow}>
+          <div className={styles.reportDetailsParam}>Project:</div>
+          <Link to={projectLink} className={styles.reportDetailsValue}>{projectName}</Link>
+        </div>
+        {this.constructor.renderDetails(details)}
+      </div>
+    );
   }
 }
