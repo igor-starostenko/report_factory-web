48f13ade80d8a224293f2a8158127a70c187c369
igor-starostenko <contactigorstar@gmail.com>
2018-03-15 23:36:12 -0700

    ApiClient allow null baseUrl

--- src/api/api_helper.js
+++ src/api/api_helper.js
@@ -1,15 +1,18 @@
 class ApiHelper {
   static verifyUrl(url) {
-    if (!url.includes('http')) {
+    if (!url || !url.includes('http')) {
       throw new Error(`Invalid API_URL: ${url}`);
     }
   }
 
   static formatUrl(url) {
-    if (url[url.length - 1] === '/') {
-      return url;
+    if (url) {
+      if (url[url.length - 1] === '/') {
+        return url;
+      }
+      return `${url}/`;
     }
-    return `${url}/`;
+    return '/';
   }
 }
 
