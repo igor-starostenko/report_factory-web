ba650c88772482fd903191ab7379dbe8ca1e02e0
igor-starostenko <contactigorstar@gmail.com>
2018-09-17 01:03:07 -0700

    Style up projects page

--- src/components/project_selection.jsx
+++ src/components/project_selection.jsx
@@ -4,31 +4,21 @@ import { Line } from 'react-chartjs-2';
 import styles from './styles/ProjectSelection.css';
 
 export default class ProjectSelection extends Component {
-  getClassName() {
-    if (this.props.newProject) {
-      return `${styles.project} ${styles.newProject}`;
-    }
-    return styles.project;
-  }
-
-  getTitleClassName() {
-    if (this.props.newProject) {
-      return styles.newProjectTitle;
-    }
-    return styles.projectTitle;
-  }
-
   render() {
-    const { description, path, name } = this.props;
+    const {
+      chartOptions, description, getChartData, name, path, style,
+    } = this.props;
     return (
-      <Link to={path} className={this.getClassName()}>
-        <Line
-          data={this.props.getChartData(name)}
-          options={this.props.chartOptions}
-        />
-        <div className={styles.projectBody}>
-          <div className={this.getTitleClassName()}>{name}</div>
-          <p className={styles.projectText}>{description}</p>
+      <Link to={path} className={styles.project} style={this.props.style}>
+        <div className={styles.projectHeader}>
+          <div className={styles.projectTitle}>{name}</div>
+          <p className={styles.projectDescription}>{description}</p>
+        </div>
+        <div className={styles.projectChart}>
+          <Line
+            data={this.props.getChartData}
+            options={this.props.chartOptions}
+          />
         </div>
       </Link>
     );
--- src/containers/projects.jsx
+++ src/containers/projects.jsx
@@ -4,7 +4,7 @@ import _ from 'lodash';
 import { Button, ProjectSelection } from '../components';
 import { queryProjects } from '../actions/projects_actions';
 import { getColors, lastDays, formatDates, reportsPerDay, reportsCreatedDates,
-  setOpacity, validateInteger } from '../helpers/chart_helpers';
+  setOpacity, validatePositive } from '../helpers/chart_helpers';
 import detailsStyles from './styles/Details.css';
 import styles from './styles/Projects.css';
 
@@ -15,10 +15,25 @@ const dataForDays = (reports, dates) => {
 
 const colors = getColors();
 
+const days = 7;
+
+const formatLabels = (daysArray) => {
+  const weekdays = formatDates(daysArray, { weekday: 'short' });
+  return weekdays.map(weekday => weekday.toUpperCase());
+}
+
+const formatTooltip = ({ datasetIndex, index }, { datasets }) => {
+  const dataset = datasets[datasetIndex];
+  const number = dataset.data[index];
+  if (number !== 1) {
+    return ` ${number} reports`;
+  }
+  return ` 1 report`;
+};
+
 const chartOptions = {
   animation: { duration: 0 },
-  elements: { point: { radius: 0 } },
-  layout: { padding: { top: 10, bottom: 10 } },
+  elements: { point: { radius: 2 } },
   legend: { display: false },
   maintainAspectRatio: false,
   responsive: true,
@@ -26,17 +41,31 @@ const chartOptions = {
     yAxes: [{
       ticks: {
          beginAtZero: true,
-         callback: validateInteger,
+         callback: validatePositive,
+         fontColor: 'rgba(0, 0, 0, 0.3)',
       },
-      gridLines: { drawBorder: false },
-      display: false,
+      gridLines: {
+        drawBorder: false,
+        color: "transparent",
+        zeroLineColor: "transparent",
+      },
     }],
     xAxes: [{
-      gridLines: { drawBorder: false },
-      display: false,
+      gridLines: {
+        drawBorder: false,
+        color: setOpacity(colors.grey, 0.5),
+      },
     }]
   },
-  tooltips: { enabled: false },
+  tooltips: {
+    callbacks: {
+      label: formatTooltip,
+    },
+    itemSort: (a, b) => a.datasetIndex > b.datasetIndex ? -1 : 1,
+    mode: 'point',
+    bodyFontSize: 14,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
 };
 
 class Projects extends Component {
@@ -46,34 +75,53 @@ class Projects extends Component {
   }
 
   componentDidMount() {
-    this.props.queryProjects(this.props.xApiKey, { lastDays: 3 });
+    this.props.queryProjects(this.props.xApiKey, { lastDays: days });
   }
 
-  getChartData(projectName) {
-    const { reports } = this.props.projects[projectName];
-    const data = dataForDays(reports, lastDays(3));
-    return {
-      labels: formatDates(lastDays(3)),
-      datasets: [
-        {
-          borderColor: setOpacity(colors.grey, 1),
-          fill: false,
-          data,
-        }
-      ]
-    }
+  getChartData(projectName, color) {
+    return (canvas) => {
+      const ctx = canvas.getContext("2d");
+  		const gradient = ctx.createLinearGradient(0,0,0,250);
+      gradient.addColorStop(0, setOpacity(color, 0.6));
+      gradient.addColorStop(1, setOpacity(color, 0));
+      const { reports } = this.props.projects[projectName];
+      const lastDates = lastDays(days);
+      return {
+        labels: formatLabels(lastDates),
+        datasets: [
+          {
+            backgroundColor: gradient,
+            borderColor: setOpacity(color, 0.3),
+            fill: 'origin',
+            pointBackgroundColor: setOpacity(color, 0.9),
+            pointHoverBackgroundColor: color,
+            data: dataForDays(reports, lastDates),
+          }
+        ]
+      }
+    };
   }
 
   renderProjects() {
+    let colorIndex = 0;
+    const colorNames = Object.keys(colors);
     return Object.keys(this.props.projects).map((projectName) => {
       const projectPath = `/projects/${projectName}`;
+      const reportsCount = this.props.projects[projectName].reports.length;
+      const description = `${reportsCount} reports for the last ${days} days`;
+      const color = colors[colorNames[colorIndex]];
+      /* eslint-disable no-unused-expressions */
+      colorIndex === colorNames.length - 2 ? colorIndex = 0 : colorIndex += 1;
+      /* eslint-enable no-unused-expressions */
       return (
         <ProjectSelection
           chartOptions={chartOptions}
-          getChartData={this.getChartData}
+          description={description}
+          getChartData={this.getChartData(projectName, color)}
           key={projectName}
           name={projectName}
           path={projectPath}
+          style={{ backgroundColor: setOpacity(color, 0.05) }}
         />
       );
     });
