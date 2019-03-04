abd487ee6f9c07c848e8f83cf60c976952244c6f
igor-starostenko <contactigorstar@gmail.com>
2019-03-03 21:01:51 -0800

    Custom mobile sidenav and styles

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
@@ -11,6 +10,69 @@ import {
 } from 'reactstrap';
 import { LinkButton } from '.';
 import styles from './styles/Navigation.css';
+
+const NavbarLinks = () => {
+  return (
+    <Fragment>
+      <NavItem>
+        <NavLink tag="div">
+          <Link to="/projects">Projects</Link>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink tag="div">
+          <Link to="/reports">Reports</Link>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink tag="div">
+          <Link to="/scenarios">Scenarios</Link>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink tag="div">
+          <Link to="/users">Users</Link>
+        </NavLink>
+      </NavItem>
+    </Fragment>
+  );
+};
+
+const ProfileButton = props => {
+  const { isLoggedIn, userId, ...rest } = props;
+
+  return (
+    <NavItem>
+      <NavLink tag="div">
+        {isLoggedIn && userId ? (
+          <LinkButton
+            className={styles.profileButton}
+            to={`/users/${userId}`}
+            {...rest}
+          >
+            Profile
+          </LinkButton>
+        ) : (
+          <LinkButton className={styles.profileButton} to="/login" {...rest}>
+            Login
+          </LinkButton>
+        )}
+      </NavLink>
+    </NavItem>
+  );
+};
+
+const SideNav = props => {
+  const { isLoggedIn, userId, className, ...rest } = props;
+
+  return (
+    <Nav className={`${styles.sideNav} ${className}`} vertical {...rest}>
+      <NavbarLinks />
+      <div className={styles.lineSeparator} />
+      <ProfileButton isLoggedIn={isLoggedIn} userId={userId} block />
+    </Nav>
+  );
+};
 
 class Navigation extends Component {
   constructor(props) {
@@ -28,59 +90,27 @@ class Navigation extends Component {
     const { isLoggedIn, userId } = this.props;
 
     return (
-      <div>
-        <Navbar className={styles.navbarYellow} dark expand="md">
-          <NavbarBrand tag="div">
-            <Link to="/">Report Factory</Link>
-          </NavbarBrand>
-          <NavbarToggler
-            className={styles.navbarToggle}
-            onClick={this.toggle}
+      <Navbar className={styles.yellow} dark expand="md">
+        <NavbarBrand tag="div">
+          <Link to="/">Report Factory</Link>
+        </NavbarBrand>
+        <NavbarToggler className={styles.navbarToggle} onClick={this.toggle} />
+        <div className={`row align-items-center ${styles.navbarDesktop}`}>
+          <Nav className="mr-auto" navbar>
+            <NavbarLinks />
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <ProfileButton isLoggedIn={isLoggedIn} userId={userId} />
+          </Nav>
+        </div>
+        {this.state.isOpen && (
+          <SideNav
+            className={styles.yellow}
+            isLoggedIn={isLoggedIn}
+            userId={userId}
           />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink tag="div">
-                  <Link to="/projects">Projects</Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag="div">
-                  <Link to="/reports">Reports</Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag="div">
-                  <Link to="/scenarios">Scenarios</Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag="div">
-                  <Link to="/users">Users</Link>
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag="div">
-                  {isLoggedIn && userId ? (
-                    <LinkButton
-                      className={styles.profileButton}
-                      to={`/users/${userId}`}
-                    >
-                      Profile
-                    </LinkButton>
-                  ) : (
-                    <LinkButton className={styles.profileButton} to="/login">
-                      Login
-                    </LinkButton>
-                  )}
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+        )}
+      </Navbar>
     );
   }
 }
