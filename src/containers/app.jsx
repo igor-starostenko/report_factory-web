b21ae8b3d577b99e865dda2527b36e0b8ebfa3cd
igor-starostenko <contactigorstar@gmail.com>
2019-03-04 20:21:13 -0800

    Reactstrap Container

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,22 +1,24 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { Container } from 'reactstrap';
 import Cookies from 'js-cookie';
-import _ from 'lodash';
+import getValue from 'lodash/get';
+import isEmpty from 'lodash/isEmpty';
 import Navbar from '../components/navigation';
 
 class App extends Component {
   isLoggedIn() {
     const xApiKey = this.props.xApiKey || Cookies.get('X-API-KEY');
-    return !_.isEmpty(xApiKey);
+    return !isEmpty(xApiKey);
   }
 
   render() {
-    const userId = _.get(this.props.currentUser, 'data.id');
+    const userId = getValue(this.props.currentUser, 'data.id');
 
     return (
       <Fragment>
         <Navbar isLoggedIn={this.isLoggedIn()} userId={userId} />
-        <div className="container">{this.props.children}</div>
+        <Container>{this.props.children}</Container>
       </Fragment>
     );
   }
