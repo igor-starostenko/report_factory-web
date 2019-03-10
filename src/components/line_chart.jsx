635b35c094723da9429556ebccf72d5a0fad2278
igor-starostenko <contactigorstar@gmail.com>
2019-03-09 22:44:26 -0800

    Styled filter buttons

--- src/components/line_chart.jsx
+++ src/components/line_chart.jsx
@@ -1,48 +1,54 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
+import { PropTypes } from 'prop-types';
 import { Line } from 'react-chartjs-2';
+import { ButtonGroup } from 'reactstrap';
 import { FilterButton } from '.';
 
-export default class LineChart extends Component {
-  constructor(props) {
-    super(props);
-    this.setFilter = this.setFilter.bind(this);
+export default function LineChart(props) {
+  const {
+    activeFilter,
+    filterMapping,
+    filterAction,
+    options,
+    getChartData,
+  } = props;
+
+  function setFilter(filterName) {
+    const newFilters = { ...filterMapping[filterName], filterName };
+    return filterAction(newFilters);
   }
 
-  setFilter(filterName) {
-    const newFilters = { ...this.props.filterMapping[filterName], filterName };
-    this.props.filterAction(newFilters);
-  }
-
-  activeFilter(filterName) {
-    return this.props.activeFilter === filterName;
-  }
-
-  renderFilterItems() {
-    return Object.keys(this.props.filterMapping).map(filterName => {
-      return (
-        <FilterButton
-          action={this.setFilter}
-          active={this.activeFilter(filterName)}
-          key={filterName}
-          name={filterName}
-          value={filterName}
-        />
-      );
-    });
-  }
-
-  render() {
-    const { options, getChartData } = this.props;
-
-    return (
-      <div>
-        <Line data={getChartData()} options={options} />
-        <div className="filters">
-          <ul id="chart-pills" className="nav nav-pills ct-orange">
-            {this.renderFilterItems()}
-          </ul>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <Fragment>
+      <Line data={getChartData()} options={options} />
+      <ButtonGroup className="filters">
+        {Object.keys(filterMapping).map(name => {
+          return (
+            <FilterButton
+              active={activeFilter === name}
+              key={name}
+              onClick={setFilter}
+              value={name}
+            >
+              {name}
+            </FilterButton>
+          );
+        })}
+      </ButtonGroup>
+    </Fragment>
+  );
 }
+
+LineChart.propTypes = {
+  activeFilter: PropTypes.string.isRequired,
+  filterMapping: PropTypes.shape({
+    Month: PropTypes.object.isRequired,
+    Week: PropTypes.object.isRequired,
+    Year: PropTypes.object.isRequired,
+  }).isRequired,
+  filterAction: PropTypes.func.isRequired,
+  options: PropTypes.shape({
+    responsive: PropTypes.bool.isRequired,
+  }).isRequired,
+  getChartData: PropTypes.func.isRequired,
+};
--- src/components/per_page_filter.jsx
+++ src/components/per_page_filter.jsx
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
+import React, { Fragment } from 'react';
+import { PropTypes } from 'prop-types';
+import { ButtonGroup } from 'reactstrap';
 import { FilterButton } from '.';
 
-export default class PerPageFilter extends Component {
-  activeFilter(number) {
-    return this.props.perPage === number;
+export default function PerPageFilter(props) {
+  const { action, perPage, totalCount, buttons } = props;
+
+  if (totalCount <= 10) {
+    return <Fragment />;
   }
 
-  renderFilterButtons() {
-    return _.map(this.props.buttons, value => {
-      const name = `${value} Per Page`;
-      return (
-        <FilterButton
-          action={this.props.action}
-          active={this.activeFilter(value)}
-          key={value}
-          name={name}
-          value={{ perPage: value }}
-        />
-      );
-    });
-  }
-
-  render() {
-    if (this.props.totalCount <= 10) {
-      return <div />;
-    }
-    return (
-      <div className="filters">
-        <ul id="chart-pills" className="nav nav-pills ct-orange">
-          {this.renderFilterButtons()}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <ButtonGroup className="filters">
+      {buttons.map(value => {
+        const name = `${value} Per Page`;
+        return (
+          <FilterButton
+            active={perPage === value}
+            key={value}
+            onClick={action}
+            value={{ perPage: value }}
+          >
+            {name}
+          </FilterButton>
+        );
+      })}
+    </ButtonGroup>
+  );
 }
+
+PerPageFilter.propTypes = {
+  action: PropTypes.func.isRequired,
+  perPage: PropTypes.number.isRequired,
+  totalCount: PropTypes.number.isRequired,
+  buttons: PropTypes.arrayOf(PropTypes.any.isRequired).isRequired,
+};
