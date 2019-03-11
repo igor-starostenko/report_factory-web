cfaba2632eeba0f32f6b24432ba73691df6f6d59
igor-starostenko <contactigorstar@gmail.com>
2019-03-10 20:50:16 -0700

    Fixed project Line chart layout

--- src/components/project_selection.jsx
+++ src/components/project_selection.jsx
@@ -3,22 +3,20 @@ import { Link } from 'react-router-dom';
 import { Line } from 'react-chartjs-2';
 import styles from './styles/ProjectSelection.css';
 
-export default class ProjectSelection extends Component {
-  render() {
-    const { description, name, path } = this.props;
-    return (
-      <Link to={path} className={styles.project} style={this.props.style}>
-        <div className={styles.projectHeader}>
-          <div className={styles.projectTitle}>{name}</div>
-          <p className={styles.projectDescription}>{description}</p>
-        </div>
-        <div className={styles.projectChart}>
-          <Line
-            data={this.props.getChartData}
-            options={this.props.chartOptions}
-          />
-        </div>
-      </Link>
-    );
-  }
+export default function ProjectSelection(props) {
+  const { description, name, path } = props;
+  return (
+    <Link to={path} className={styles.project} style={props.style}>
+      <div className={styles.projectHeader}>
+        <div className={styles.projectTitle}>{name}</div>
+        <p className={styles.projectDescription}>{description}</p>
+      </div>
+      <div className={styles.projectChart}>
+        <Line
+          data={props.getChartData}
+          options={props.chartOptions}
+        />
+      </div>
+    </Link>
+  );
 }
--- src/containers/projects.jsx
+++ src/containers/projects.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-import _ from 'lodash';
-import { Button, ProjectSelection } from '../components';
+import getValue from 'lodash/get';
+import { Button, Loading, ProjectSelection } from '../components';
 import { queryProjects } from '../actions/projects_actions';
 import {
   getColors,
@@ -15,28 +16,28 @@ import {
 import detailsStyles from './styles/Details.css';
 import styles from './styles/Projects.css';
 
-const dataForDays = (reports, dates) => {
+function dataForDays(reports, dates) {
   const reportsDates = reportsCreatedDates(reports, report => report.createdAt);
   return reportsPerDay(dates, reportsDates);
-};
+}
 
 const colors = getColors();
-
+const colorNames = Object.keys(colors);
 const days = 7;
 
-const formatLabels = daysArray => {
+function formatLabels(daysArray) {
   const weekdays = formatDates(daysArray, { weekday: 'short' });
   return weekdays.map(weekday => weekday.toUpperCase());
-};
+}
 
-const formatTooltip = ({ datasetIndex, index }, { datasets }) => {
+function formatTooltip({ datasetIndex, index }, { datasets }) {
   const dataset = datasets[datasetIndex];
   const number = dataset.data[index];
   if (number !== 1) {
     return ` ${number} reports`;
   }
   return ` 1 report`;
-};
+}
 
 const chartOptions = {
   animation: { duration: 0 },
@@ -79,23 +80,20 @@ const chartOptions = {
   },
 };
 
-class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.getChartData = this.getChartData.bind(this);
-  }
+function Projects(props) {
+  const { xApiKey, projects, isAdmin, loaded } = props;
 
-  componentDidMount() {
-    this.props.queryProjects(this.props.xApiKey, { lastDays: days });
-  }
+  useEffect(() => {
+    props.queryProjects(xApiKey, { lastDays: days });
+  }, []);
 
-  getChartData(projectName, color) {
+  function getChartData(projectName, color) {
     return canvas => {
       const ctx = canvas.getContext('2d');
       const gradient = ctx.createLinearGradient(0, 0, 0, 250);
       gradient.addColorStop(0, setOpacity(color, 0.6));
       gradient.addColorStop(1, setOpacity(color, 0));
-      const { reports } = this.props.projects[projectName];
+      const { reports } = projects[projectName];
       const lastDates = lastDays(days);
       return {
         labels: formatLabels(lastDates),
@@ -113,63 +111,71 @@ class Projects extends Component {
     };
   }
 
-  renderProjects() {
-    let colorIndex = 0;
-    const colorNames = Object.keys(colors);
-    return Object.keys(this.props.projects).map(projectName => {
-      const projectPath = `/projects/${projectName}`;
-      const reportsCount = this.props.projects[projectName].reports.length;
-      const description = `${reportsCount} reports for the last ${days} days`;
-      const color = colors[colorNames[colorIndex]];
-      /* eslint-disable no-unused-expressions */
-      colorIndex === colorNames.length - 2
-        ? (colorIndex = 0)
-        : (colorIndex += 1);
-      /* eslint-enable no-unused-expressions */
-      return (
-        <ProjectSelection
-          chartOptions={chartOptions}
-          description={description}
-          getChartData={this.getChartData(projectName, color)}
-          key={projectName}
-          name={projectName}
-          path={projectPath}
-          style={{ backgroundColor: setOpacity(color, 0.05) }}
-        />
-      );
-    });
+  if (!loaded) {
+    return <Loading />;
   }
 
-  render() {
-    if (!this.props.loaded) {
-      return <div className="loading">Loading...</div>;
-    }
-
-    return (
-      <div>
-        <br />
-        <div className={detailsStyles.detailsContainer}>
-          <div className={detailsStyles.detailsHeader}>
-            <div className={detailsStyles.detailsName}>Projects</div>
-          </div>
-          <div className={detailsStyles.detailsButtons}>
-            {this.props.isAdmin && (
-              <Button color="primary" fill="true" to="/project/new">
-                Add a Project
-              </Button>
-            )}
-          </div>
-          <div className={styles.projectsList}>{this.renderProjects()}</div>
+  let colorIndex = 0;
+  return (
+    <Fragment>
+      <br />
+      <div className={detailsStyles.detailsContainer}>
+        <div className={detailsStyles.detailsHeader}>
+          <div className={detailsStyles.detailsName}>Projects</div>
+        </div>
+        <div className={detailsStyles.detailsButtons}>
+          {isAdmin && (
+            <Button color="primary" fill="true" to="/project/new">
+              Add a Project
+            </Button>
+          )}
+        </div>
+        <div className={styles.projectsList}>
+          {Object.keys(projects).map(projectName => {
+            const projectPath = `/projects/${projectName}`;
+            const reportsCount = props.projects[projectName].reports.length;
+            const description = `${reportsCount} reports for the last ${days} days`;
+            const color = colors[colorNames[colorIndex]];
+            /* eslint-disable no-unused-expressions */
+            colorIndex === colorNames.length - 2
+              ? (colorIndex = 0)
+              : (colorIndex += 1);
+            /* eslint-enable no-unused-expressions */
+            return (
+              <ProjectSelection
+                chartOptions={chartOptions}
+                description={description}
+                getChartData={getChartData(projectName, color)}
+                key={projectName}
+                name={projectName}
+                path={projectPath}
+                style={{ backgroundColor: setOpacity(color, 0.05) }}
+              />
+            );
+          })}
         </div>
       </div>
-    );
-  }
+    </Fragment>
+  );
 }
+
+Projects.propTypes = {
+  xApiKey: PropTypes.string.isRequired,
+  projects: PropTypes.shape({
+    [PropTypes.string]: PropTypes.shape({
+      reports: PropTypes.array.isRequired,
+    }),
+  }).isRequired,
+  isAdmin: PropTypes.bool.isRequired,
+  loaded: PropTypes.bool.isRequired,
+  queryProjects: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({
   projects: state.projects.list.data,
   loaded: state.projects.list.loaded,
-  isAdmin: _.get(state.users.currentUser, 'data.attributes.type') === 'Admin',
+  isAdmin:
+    getValue(state.users.currentUser, 'data.attributes.type') === 'Admin',
   xApiKey: state.users.currentUser.xApiKey,
 });
 
