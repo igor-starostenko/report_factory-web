c2217e2f835ae88a1ca424a7a6d231958dd573cc
igor-starostenko <contactigorstar@gmail.com>
2019-03-11 03:26:17 -0700

    ProjectSelection function component with prop types

--- src/components/project_selection.jsx
+++ src/components/project_selection.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Line } from 'react-chartjs-2';
 import styles from './styles/ProjectSelection.css';
@@ -12,11 +13,31 @@ export default function ProjectSelection(props) {
         <p className={styles.projectDescription}>{description}</p>
       </div>
       <div className={styles.projectChart}>
-        <Line
-          data={props.getChartData}
-          options={props.chartOptions}
-        />
+        <Line data={props.getChartData} options={props.chartOptions} />
       </div>
     </Link>
   );
 }
+
+ProjectSelection.propTypes = {
+  description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  getChartData: PropTypes.func.isRequired,
+  chartOptions: PropTypes.shape({
+    maintainAspectRatio: PropTypes.bool.isRequired,
+    responsive: PropTypes.bool.isRequired,
+    scales: PropTypes.shape({
+      yAxes: PropTypes.array,
+      xAxes: PropTypes.array,
+    }).isRequired,
+    tooltips: PropTypes.object,
+  }).isRequired,
+  style: PropTypes.shape({
+    backgroundColor: PropTypes.string,
+  }),
+};
+
+ProjectSelection.defaultProps = {
+  style: {},
+};
