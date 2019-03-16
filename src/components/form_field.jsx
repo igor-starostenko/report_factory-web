2505c3da6dada0d7495ba35418d5fdb48794c721
igor-starostenko <contactigorstar@gmail.com>
2019-03-16 12:35:11 -0700

    more functional components

--- src/components/form_field.jsx
+++ src/components/form_field.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Input, FormGroup, FormFeedback, Label } from 'reactstrap';
 
-export default class FormField extends Component {
-  render() {
-    const {
-      meta: { touched, error },
-      label,
-      placeholder,
-      input,
-      ...rest
-    } = this.props;
+export default function FormField(props) {
+  const {
+    meta: { touched, error },
+    label,
+    placeholder,
+    input,
+    ...rest
+  } = props;
 
-    return (
-      <FormGroup>
-        <Label htmlFor={label}>{label}</Label>
-        <Input
-          id={label}
-          placeholder={placeholder}
-          invalid={touched && !!error}
-          valid={touched && !error}
-          {...input}
-          {...rest}
-        />
-        {touched && <FormFeedback>{error}</FormFeedback>}
-      </FormGroup>
-    );
-  }
+  return (
+    <FormGroup>
+      <Label htmlFor={label}>{label}</Label>
+      <Input
+        id={label}
+        placeholder={placeholder}
+        invalid={touched && !!error}
+        valid={touched && !error}
+        {...input}
+        {...rest}
+      />
+      {touched && <FormFeedback>{error}</FormFeedback>}
+    </FormGroup>
+  );
 }
 
 FormField.propTypes = {
--- src/components/not_found.jsx
+++ src/components/not_found.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 
-export default class NotFound extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Not Found</h1>
-      </div>
-    );
-  }
+export default function NotFound(props) {
+  return (
+    <Fragment>
+      <h1 {...props}>Not Found</h1>
+    </Fragment>
+  );
 }
