540befbd86e8f171be028883cf980e9e74854585
igor-starostenko <contactigorstar@gmail.com>
2019-03-15 22:44:22 -0700

    Search Reports with hooks

--- src/containers/search_reports.jsx
+++ src/containers/search_reports.jsx
@@ -1,73 +1,85 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import { PropTypes } from 'prop-types';
 import { Field, reduxForm, reset } from 'redux-form';
 import { connect } from 'react-redux';
-import _ from 'lodash';
+import isEmpty from 'lodash/isEmpty';
+import merge from 'lodash/merge';
+import pick from 'lodash/pick';
+import split from 'lodash/split';
 import { Button } from 'reactstrap';
 import styles from '../components/styles/SearchBar.css';
 
 const formatTags = tags => {
-  if (_.isEmpty(tags)) {
+  if (isEmpty(tags)) {
     return { tags: [] };
   }
   const noSpecialCharsString = tags.replace(/[^\-\w\s]/gi, '');
-  return { tags: _.split(noSpecialCharsString, ' ') };
+  return { tags: split(noSpecialCharsString, ' ') };
 };
 
-class SearchReports extends Component {
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.resetSearch = this.resetSearch.bind(this);
+function SearchReports(props) {
+  useEffect(
+    () => () => {
+      props.reset('searchReportsForm');
+      props.setSearch([]);
+    },
+    [],
+  );
+
+  function onSubmit(searchInput) {
+    let options = pick(props, ['page', 'perPage']);
+    options = merge(options, formatTags(searchInput.tags));
+    props.setSearch(options.tags);
+    return props.action(options);
   }
 
-  componentWillUnmount() {
-    this.props.reset('searchReportsForm');
-    this.props.setSearch([]);
+  function resetSearch() {
+    props.reset('searchReportsForm');
+    onSubmit({ tags: [] });
   }
 
-  onSubmit(searchInput) {
-    let options = _.pick(this.props, ['page', 'perPage']);
-    options = _.merge(options, formatTags(searchInput.tags));
-    this.props.setSearch(options.tags);
-    return this.props.action(options);
-  }
+  const hasTags = !isEmpty(props.values.tags);
+  const cancelClass = hasTags ? 'cancelEnabled' : 'cancelHidden';
 
-  resetSearch() {
-    this.props.reset('searchReportsForm');
-    this.onSubmit({ tags: [] });
-  }
-
-  render() {
-    const hasTags = !_.isEmpty(this.props.values.tags);
-    const cancelClass = hasTags ? 'cancelEnabled' : 'cancelHidden';
-    return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className={`${styles.searchForm} form-control`}
+  return (
+    <form
+      onSubmit={props.handleSubmit(onSubmit)}
+      className={`${styles.searchForm} form-control`}
+    >
+      <Field
+        className={styles.searchField}
+        placeholder="Search reports by tags"
+        name="tags"
+        type="text"
+        component="input"
+      />
+      <button
+        className={styles[cancelClass]}
+        onClick={resetSearch}
+        type="button"
       >
-        <Field
-          className={styles.searchField}
-          placeholder="Search reports by tags"
-          name="tags"
-          type="text"
-          component="input"
-        />
-        <button
-          className={styles[cancelClass]}
-          onClick={this.resetSearch}
-          type="button"
-        >
-          X
-        </button>
-        <div className={styles.searchButton}>
-          <Button type="submit" color="primary">
-            Search
-          </Button>
-        </div>
-      </form>
-    );
-  }
+        X
+      </button>
+      <div className={styles.searchButton}>
+        <Button type="submit" color="primary">
+          Search
+        </Button>
+      </div>
+    </form>
+  );
 }
+SearchReports.propTypes = {
+  values: PropTypes.shape({
+    tags: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.string),
+      PropTypes.string,
+    ]),
+  }).isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  reset: PropTypes.func.isRequired,
+  action: PropTypes.func.isRequired,
+  setSearch: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({
   values: state.form.searchReportsForm.values,
